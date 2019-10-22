f8b89f9c6b1b8dbc220a665ffc7afb5115364573
ramithaJHU <rami@outlook.com.au>
2019-10-22 19:48:24 +0530

    border removed

--- plot4.R
+++ plot4.R
@@ -37,7 +37,7 @@ plot(date_and_time, voltage, type="l", xlab="datetime", ylab="Voltage")
 plot(date_and_time, sub_metering_1, type="l", ylab="Energy Sub metering", xlab="")
 lines(date_and_time, sub_metering_2, type="l", col="red")
 lines(date_and_time, sub_metering_3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="n")
 
 plot(date_and_time, global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 
